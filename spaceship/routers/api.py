b6ba8ee8ec6758085e1ca1f6d6ad5afebb41d908
JestAK <Kaliberda.Anton@LLL.kpi.ua>
2025-06-06 05:17:39 +0200

    Added endpoint for matrix production and added numpy library

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix")
+def matrix() -> dict:
+    mat_a = np.random.randint(0, 10, size=(10, 10))
+    mat_b = np.random.randint(0, 10, size=(10, 10))
+
+    product = mat_a.dot(mat_b)
+
+    return {
+        "matrix_a": mat_a.tolist(),
+        "matrix_b": mat_b.tolist(),
+        "product":   product.tolist(),
+    }
